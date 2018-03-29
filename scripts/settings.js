41ded9bac6d1f97b51195ff5f2456b12b8a33efe
Tobias Løfgren <tobloef@gmail.com>
2018-03-29 21:14:35 +0200

    Update settings.js

--- scripts/settings.js
+++ scripts/settings.js
@@ -5,7 +5,7 @@ settings = (function() {
 	
 	// Default values for various user settings.
 	const defaultValues = {
-		"documentContent": "Text2MindMap\n\tTurn tab-indented lists into mind \n\t\tPress Tab to indent lines\n\t\tPress Shift + Tab to unindent lines\n\tDrag nodes to re-organize them\n\tThis project is based on the now dead site Text2MindMap.com",
+		"documentContent": "Text2MindMap\n\tTurn tab-indented lists into mind maps\n\t\tPress Tab to indent lines\n\t\tPress Shift + Tab to unindent lines\n\tDrag nodes to re-organize them\n\tThis project is based on the now dead site Text2MindMap.com",
 		"documentTitle": "Untitled Document"
 	};
 
